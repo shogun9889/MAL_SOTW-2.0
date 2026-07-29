cd411a7afa6f0574dff8d5ac5ad952ec5913f3cb
shogun9889 <ayat.seddik123@gmail.com>
2026-07-29 11:44:23 +0100

    Add files via upload

---
+++ data.js
@@ -0,0 +1,122 @@
+const songDatabase = [
+    {
+        startDate: "06-04-2026",
+        endDate: "12-04-2026",
+        submitter: "Shogun",
+        title: "Yakusoku by Suga Shikao",
+        link: "https://youtu.be/I8KDSIquUqU?si=BDLRbqp9LNinsRDm", 
+        meanScore: "N/A"
+    },
+    {
+        startDate: "13-04-2026",
+        endDate: "19-04-2026",
+        submitter: "B1G2B3N",
+        title: "TRIAD FORCE by ORCHERIA",
+        link: "https://youtu.be/P5_ZwfVvdfs?is=7Sn-RGtD_inL3ytW",
+        meanScore: "N/A"
+    },
+    {
+        startDate: "20-04-2026",
+        endDate: "26-04-2026",
+        submitter: "Jokobo",
+        title: "Hakanairensa by Asaka",
+        link: "https://www.youtube.com/watch?v=-oP-wpPLxMg",
+        meanScore: "N/A"
+    },
+    {
+        startDate: "27-04-2026",
+        endDate: "03-05-2026",
+        submitter: "Duck",
+        title: "New World by Maika",
+        link: "https://youtu.be/9m3qeiAgZvA?si=Jf4D1GgAHMVtVR7J",
+        meanScore: "N/A"
+    },
+    {
+        startDate: "04-05-2026",
+        endDate: "10-05-2026",
+        submitter: "Lady Avalon",
+        title: "Love In The Ice by TVXQ",
+        link: "https://youtu.be/5A1ddq_VVNc?si=JSxp8P77VmxBpqmP",
+        meanScore: "N/A"
+    },
+    {
+        startDate: "11-05-2026",
+        endDate: "17-05-2026",
+        submitter: "Naru",
+        title: "Kawaki wo Ameku by Minami",
+        link: "https://youtu.be/0YF8vecQWYs?si=S5XaYQGJQSpFcpBd",
+        meanScore: "N/A"
+    },
+    {
+        startDate: "18-05-2026",
+        endDate: "24-05-2026",
+        submitter: "Shogun",
+        title: "F by MAXIMUM THE HORMONE",
+        link: "https://youtu.be/_SnyXfvCCGo?si=7pXbVJ1tKVMkORxh",
+        meanScore: "N/A"
+    },
+    {
+        startDate: "01-06-2026",
+        endDate: "07-06-2026",
+        submitter: "B1G2B3N",
+        title: "yezzir by bbno$",
+        link: "https://youtu.be/oFP6tss8ODE?is=6s9jvkLAjFP0HHON",
+        meanScore: "N/A"
+    },
+    {
+        startDate: "08-06-2026",
+        endDate: "14-06-2026",
+        submitter: "Jokobo",
+        title: "KING OF EVIL by TENROGUN",
+        link: "https://www.youtube.com/watch?v=0kXwWcPoppk%20YouTube",
+        meanScore: "N/A"
+    },
+    {
+        startDate: "22-06-2026",
+        endDate: "28-06-2026",
+        submitter: "Duck",
+        title: "Ma Vie, Mes Rêves by Shiina Ringo",
+        link: "https://youtu.be/GDnLHd2Ei3Y?si=InoWpLMk-8kspOaF",
+        meanScore: "3.27/5"
+    },
+    {
+        startDate: "29-06-2026",
+        endDate: "05-07-2026",
+        submitter: "Lady Avalon",
+        title: "Gelande ga Tokeru Hodo Koi Shitai by Tachibana Hinano",
+        link: "https://youtu.be/ZGIu8i8-YUU?is=q7uCinS24JV-_FAe",
+        meanScore: "3.13/5"
+	},
+    {
+        startDate: "06-07-2026",
+        endDate: "12-07-2026",
+        submitter: "Naru",
+        title: "ReawakeR by LiSA feat Felix of Stray Kids",
+        link: "https://youtu.be/oe4XJ851oOk?si=stTFcK4IUt14PDLs",
+        meanScore: "2.1/5"
+    },
+	{
+        startDate: "13-07-2026",
+        endDate: "19-07-2026",
+        submitter: "Shogun",
+        title: "Namae No Nai Kaibutsu by EGOIST",
+        link: "https://youtu.be/oH25LwzUYTU?si=Yb5RrgQ86SmUBkoc",
+        meanScore: "4.0/5"
+    },
+	{
+        startDate: "20-07-2026",
+        endDate: "26-07-2026",
+        submitter: "B1G2B3N",
+        title: "Recognize by Livingston",
+        link: "https://youtu.be/xC-q2Irz8d8?is=Z-zgbR8ORoTcChTW",
+        meanScore: "4.0/5"
+    },
+	{
+        startDate: "27-07-2026",
+        endDate: "02-08-2026",
+        submitter: "Jokobo",
+        title: "Hoshi no Arika by Falcom Sound Team JDK (the vocalist is Hideaki Hamada)",
+        link: "https://www.youtube.com/watch?v=rQ_1X3fDn-M",
+        meanScore: "N/A"
+    },
+];
